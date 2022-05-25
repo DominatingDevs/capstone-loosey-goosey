623f601460f5ab83bde0e8890e63de5a26eb5f8a
Van Dexter <vanguardstellar@gmail.com>
2022-05-25 13:30:54 -0700

    first

---
+++ app/javascript/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+class App extends React.Component {
+  render () {
+    return (
+
+    );
+  }
+}
+
+export default App
