7524fcc3fb31f970d0d439d2d520ee627e39a546
VMMARIN <vanessam.marin@gmail.com>
2022-06-07 09:52:32 -0700

    fixedindentation

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -16,46 +16,41 @@ class Header extends Component {
             sign_out_route
         } = this.props
         return (
-    <>
-    <div class="logonav">
-<a id='short' href="/"><img className="homebutton" src={homebutton} scale="0"></img></a> 
-     </div>
-        <div className="listview-title mt-2"> 
+            <>
+                <div class="logonav">
+                    <a id='short' href="/"><img className="homebutton" src={homebutton} scale="0"></img></a>
+                </div>
+                <div className="listview-title mt-2">
 
-                 <NavLink to="/"> </NavLink>
-                     <Nav>
+                    <NavLink to="/"> </NavLink>
+                    <Nav>
                         <NavItem>
-                             <NavLink to="/gooseyindex">View All </NavLink>
+                            <NavLink to="/gooseyindex">View All </NavLink>
                         </NavItem>
-                            <>
-                          <NavItem>
-                             <NavLink to="/mygooseylist">My Listings</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink to="/gooseynew">Add A Listing</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink to="/aboutus">About Us</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <a href={sign_out_route}>Sign Out</a>
+                        <>
+                            <NavItem>
+                                <NavLink to="/mygooseylist">My Listings</NavLink>
                             </NavItem>
-                         </>
-                          {!logged_in &&
+                            <NavItem>
+                                <NavLink to="/gooseynew">Add A Listing</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink to="/aboutus">About Us</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <a href={sign_out_route}>Sign Out</a>
+                            </NavItem>
+                        </>
+                        {!logged_in &&
                             <NavItem>
                                 <a href={sign_in_route}>Sign In</a>
                             </NavItem>
-                            }
-                        </Nav>
-                     </div>  
-                    </>
-                )}
-            }
-
-
-
-
-
-
+                        }
+                    </Nav>
+                </div>
+            </>
+        )
+    }
+}
 
 export default Header
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import logo from '../assets/logo.png'
 
-
-
-
 export default class Home extends Component {
   render() {
     return (
-      <div className="homenav"> 
-      <a id='short' href="/gooseyindex"><img className="logo" src={logo} scale="0"></img></a> 
+      <div className="homenav">
+        <a id='short' href="/gooseyindex"><img className="logo" src={logo} scale="0"></img></a>
       </div>
-      )
-    }
+    )
   }
+}
